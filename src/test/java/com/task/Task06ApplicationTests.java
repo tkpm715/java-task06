c0668eb7319be84ebd34894902f33690cf453887
tk <pm@letter.mail-box.ne.jp>
2022-06-11 21:31:52 +0900

    最初のコミット

---
+++ src/test/java/com/task/Task06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
